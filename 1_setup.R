d2a76ad0f49924018f2498a83688d3cb62b4854b
Tony Papenfuss <papenfuss@wehi.edu.au>
2018-08-31 13:59:01 +1000

    Update 1_setup.R

--- 1_setup.R
+++ 1_setup.R
@@ -15,21 +15,3 @@ if(!require(sequenza)) {
 if(!require(stringr)) {
   install.packages("stringr", repos='http://cran.ms.unimelb.edu.au/')
 }
-
-# Check libraries and install if required
-if(!require(copynumber)) {
-  source("http://bioconductor.org/biocLite.R")
-  biocLite("copynumber")
-}
-
-if(!require(squash)) {
-  install.packages("squash", repos='http://cran.ms.unimelb.edu.au/')
-}
-
-if(!require(sequenza)) {
-  install.packages("sequenza", repos='http://cran.ms.unimelb.edu.au/')
-}
-
-if(!require(stringr)) {
-  install.packages("stringr", repos='http://cran.ms.unimelb.edu.au/')
-}
