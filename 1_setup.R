b20f3c7037ad510ba64a8fb3ff8c7e7fcfbbb984
Tony Papenfuss <papenfuss@wehi.edu.au>
2018-08-31 10:12:27 +1000

    Update 1_setup.R

--- 1_setup.R
+++ 1_setup.R
@@ -23,8 +23,3 @@ if(!require(stringr)) {
 dir.create("./BINF90004", showWarnings = TRUE, recursive = FALSE, mode = "0777")
 setwd("~/BINF90004")
 dir.create("./data", showWarnings = TRUE, recursive = FALSE, mode = "0777")
-
-# download.file("http://bioinf.wehi.edu.au/~papenfuss/BINF90004/data/CA0023-61-2_chr1_bin50.seqz.gz", "data/CA0023-61-2_chr1_bin50.seqz.gz")
-# download.file("http://bioinf.wehi.edu.au/~papenfuss/BINF90004/data/CA0023-61-3_chr1_bin50.seqz.gz", "data/CA0023-61-3_chr1_bin50.seqz.gz")
-# download.file("http://bioinf.wehi.edu.au/~papenfuss/BINF90004/data/CA0023-65_chr1_bin50.seqz.gz", "data/CA0023-65_chr1_bin50.seqz.gz")
-# download.file("http://bioinf.wehi.edu.au/~papenfuss/BINF90004/data/CA0023-66_chr1_bin50.seqz.gz", "data/CA0023-66_chr1_bin50.seqz.gz")
