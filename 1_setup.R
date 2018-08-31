4e2fd7b45706c33d5b27b714e64fecd0d9155bca
Tony Papenfuss <papenfuss@wehi.edu.au>
2018-08-31 10:18:42 +1000

    Remove directory setup code

--- 1_setup.R
+++ 1_setup.R
@@ -16,10 +16,20 @@ if(!require(stringr)) {
   install.packages("stringr", repos='http://cran.ms.unimelb.edu.au/')
 }
 
+# Check libraries and install if required
+if(!require(copynumber)) {
+  source("http://bioconductor.org/biocLite.R")
+  biocLite("copynumber")
+}
 
-## Setup directories
+if(!require(squash)) {
+  install.packages("squash", repos='http://cran.ms.unimelb.edu.au/')
+}
 
-# Change the fopllowing to the location you have downloaded the workshop directory to.
-dir.create("./BINF90004", showWarnings = TRUE, recursive = FALSE, mode = "0777")
-setwd("~/BINF90004")
-dir.create("./data", showWarnings = TRUE, recursive = FALSE, mode = "0777")
+if(!require(sequenza)) {
+  install.packages("sequenza", repos='http://cran.ms.unimelb.edu.au/')
+}
+
+if(!require(stringr)) {
+  install.packages("stringr", repos='http://cran.ms.unimelb.edu.au/')
+}
